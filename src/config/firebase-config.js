faff6a38db0eefd5ec5a5e21da24871e95a8f953
SandroGash <sandro.andriamanohera@gmail.com>
2024-05-19 12:05:25 +0200

    design system

---
+++ src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALLfv2zKzJgnIa9TPwSKTFQkBBnfIuWpY",
+  authDomain: "socit-796b8.firebaseapp.com",
+  projectId: "socit-796b8",
+  storageBucket: "socit-796b8.appspot.com",
+  messagingSenderId: "568270830546",
+  appId: "1:568270830546:web:0c52cbf8a578cb08d1260b",
+};
+
+const app = initializeApp(firebaseConfig);
