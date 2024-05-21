5823739df41a1094c18706df75b47dd3623c7181
SandroGash <sandro.andriamanohera@gmail.com>
2024-05-21 16:47:43 +0200

    Tailwind config up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,39 @@ import { Seo } from "@/ui/components/seo";
 import { Button } from "@/ui/design-system/button/button";
 import { Typography } from "@/ui/design-system/typography/typography";
 import { RiAncientGateFill, RiUser6Fill } from "react-icons/ri";
+import { Logo } from "@/ui/design-system/logo/logo";
+import { Avatar } from "@/ui/design-system/avatar/avatar";
 
 export default function Home() {
   return (
     <>
       <Seo title="socit" description="description..." />
 
-      <RiUser6Fill />
       <Typography variant="display" component="h1">
         Socit
       </Typography>
+
+      <div className="space-y-2">
+        <Typography variant="caption2" weight="medium">
+          Avatar
+        </Typography>
+        <div className="flex items-center gap-2 p-5 border border-gray-400 rounded">
+          <Avatar
+            size="small"
+            src="/asset/images/reynier-carl-wf0c0d-h2fE-unsplash.jpg"
+            alt="Avatar de Unknown Listener"
+          />
+          <Avatar
+            src="/asset/images/reynier-carl-wf0c0d-h2fE-unsplash.jpg"
+            alt="Avatar de Unknown Listener"
+          />
+          <Avatar
+            size="large"
+            src="/asset/images/reynier-carl-wf0c0d-h2fE-unsplash.jpg"
+            alt="Avatar de Unknown Listener"
+          />
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,167 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#ffffff",
+      primary: {
+        200: "#a9cce3",
+        300: "#7fb3d5",
+        400: "#5499c7",
+        DEFAULT: "#2980b9",
+        600: "#2471a3",
+      },
+      secondary: {
+        200: "#a9cce3",
+        300: "#99d4e4",
+        400: "#8fdbdb",
+        DEFAULT: "#95e1ca",
+        600: "#a9e3b4",
+      },
+      gray: {
+        400: "#f2f2f2",
+        500: "#e5e5e5",
+        600: "#b2b2b2",
+        700: "#808080",
+        800: "#333333",
+        DEFAULT: "#1D1D1D",
+      },
+    },
+    fontSize: {
+      "8xl": [
+        "120px",
+        {
+          lineHeight: "120px",
+          letterSpacing: "-6px",
+          fontWeight: "500",
+        },
+      ],
+      "7xl": [
+        "72px",
+        {
+          lineHeight: "80px",
+          letterSpacing: "-4.5px",
+          fontWeight: "600",
+        },
+      ],
+      "6xl": [
+        "55px",
+        {
+          lineHeight: "60px",
+          letterSpacing: "-2.5px",
+          fontWeight: "500",
+        },
+      ],
+      "5xl": [
+        "48px",
+        {
+          lineHeight: "54px",
+          letterSpacing: "-1.600000023841858px",
+          fontWeight: "500",
+        },
+      ],
+      "4xl": [
+        "36px",
+        {
+          lineHeight: "44px",
+          letterSpacing: "-1.2000000476837158px",
+          fontWeight: "500",
+        },
+      ],
+      "3xl": [
+        "28px",
+        {
+          lineHeight: "34px",
+          letterSpacing: "-0.800000011920929px",
+          fontWeight: "500",
+        },
+      ],
+      "2xl": [
+        "24px",
+        {
+          lineHeight: "30px",
+          letterSpacing: "-1px",
+          fontWeight: "400",
+        },
+      ],
+      xl: [
+        "24px",
+        {
+          lineHeight: "30px",
+          letterSpacing: "-1px",
+          fontWeight: "400",
+        },
+      ],
+      lg: [
+        "21px",
+        {
+          lineHeight: "30px",
+          letterSpacing: "-0.800000011920929px",
+          fontWeight: "400",
+        },
+      ],
+      base: [
+        "17px",
+        {
+          lineHeight: "25px",
+          letterSpacing: "-0.699999988079071px",
+          fontWeight: "400",
+        },
+      ],
+      sm: [
+        "15px",
+        {
+          lineHeight: "23px",
+          letterSpacing: "-0.6000000238418579px",
+          fontWeight: "400",
+        },
+      ],
+      caption1: [
+        "20px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "-0.6000000238418579px",
+          fontWeight: "400",
+        },
+      ],
+      caption2: [
+        "18px",
+        {
+          lineHeight: "20px",
+          letterSpacing: "-0.30000001192092896px",
+          fontWeight: "400",
+        },
+      ],
+      caption3: [
+        "16px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "-0.5px",
+          fontWeight: "400",
+        },
+      ],
+      caption4: [
+        "13px",
+        {
+          lineHeight: "15px",
+          letterSpacing: "-0.2000000198023224px",
+          fontWeight: "400",
+        },
+      ],
+    },
+
+    borderRadius: {
+      DEFAULT: "10px",
+      full: "9999PiX",
+    },
+
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [],
-}
-
+};
