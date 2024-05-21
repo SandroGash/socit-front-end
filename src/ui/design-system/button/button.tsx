3acc9fc21ec44ede8b00bb30df928c757913ae89
SandroGash <sandro.andriamanohera@gmail.com>
2024-05-21 11:59:03 +0200

    create spinner

--- src/ui/design-system/button/button.tsx
+++ src/ui/design-system/button/button.tsx
@@ -1,5 +1,6 @@
 import { IconProps } from "@/types/iconProps";
 import clsx from "clsx";
+import { Spinner } from "../spinner/spinner";
 
 interface Props {
   size?: "small" | "medium" | "large";
@@ -87,19 +88,36 @@ export const Button = ({
     <>
       <button
         type="button"
-        className={clsx(variantStyles, sizeStyles, icoSize)}
+        className={clsx(
+          variantStyles,
+          sizeStyles,
+          icoSize && "cursor-wait",
+          "relative"
+        )}
         onClick={() => console.log("click")}
         disabled={disabled}
       >
-        {icon && variant === "ico" ? (
-          <icon.icon size={icoSize} />
-        ) : (
-          <div className={clsx(icon && "flex items-center gap-1")}>
-            {icon && iconPosition === "left" && <icon.icon size={icoSize} />}
-            {children}
-            {icon && iconPosition === "right" && <icon.icon size={icoSize} />}
+        {isLoading && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            {variant === "accent" || variant === "ico" ? (
+              <Spinner size="small" variant="white" />
+            ) : (
+              <Spinner size="small" />
+            )}
           </div>
         )}
+
+        <div className={clsx(isLoading && "invisible")}>
+          {icon && variant === "ico" ? (
+            <icon.icon size={icoSize} />
+          ) : (
+            <div className={clsx(icon && "flex items-center gap-1")}>
+              {icon && iconPosition === "left" && <icon.icon size={icoSize} />}
+              {children}
+              {icon && iconPosition === "right" && <icon.icon size={icoSize} />}
+            </div>
+          )}
+        </div>
       </button>
     </>
   );
